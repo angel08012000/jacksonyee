b757e0a93eca3dba50f7d48c1d08a61d3bc8f6eb
Cih <angel08012000@gmail.com>
2021-01-08 00:34:55 +0800

    Initial Commit

---
+++ jacksonyee/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  jacksonyee
+//
+//  Created by 慈慈 on 2021/1/8.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ jacksonyee/jacksonyeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  jacksonyeeApp.swift
+//  jacksonyee
+//
+//  Created by 慈慈 on 2021/1/8.
+//
+
+import SwiftUI
+
+@main
+struct jacksonyeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
